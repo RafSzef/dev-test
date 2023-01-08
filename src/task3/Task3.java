f7e869b0ab207c83179293c66e5d67cb267259c3
Rafał Szefler <59801632+RafSzef@users.noreply.github.com>
2023-01-08 20:15:56 +0100

    task 3 commented

--- src/task3/Task3.java
+++ src/task3/Task3.java
@@ -6,8 +6,10 @@ import java.util.List;
 public class Task3 {
 
     public static void main(String[] args) {
+        //HashMap stores verticle and all connected neighbour verticles
         HashMap<Integer, List<Integer>> inputMap = GraphScanner.scan();
         GraphCounter graphCounter = new GraphCounter();
+        //graphCounter counts all sub-graphs, then prints them on console
         System.out.println(graphCounter.countGraphs(inputMap));
     }
 }
