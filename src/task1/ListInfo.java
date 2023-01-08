9f96b85d18d7379cd2b7b80a65f7ad08ca6f0c75
Rafał Szefler <59801632+RafSzef@users.noreply.github.com>
2023-01-08 19:11:57 +0100

    task 1 commented

--- src/task1/ListInfo.java
+++ src/task1/ListInfo.java
@@ -5,9 +5,19 @@ import java.util.InputMismatchException;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Class used to get all info needed in task 1.
+ * All methods except getAllInfo are private.
+ * All methods return values - ready for unit test
+ */
 public class ListInfo {
 
-    public String getAllInfo (List<Integer> list) {
+    /**
+     * @param list - list of numbers [Integer]
+     * @return String formatted for the task
+     */
+    public String getAllInfo(List<Integer> list) {
+        //Use of String.format allowed me to add many parameters in a readable way
         return String.format("%s\ncount: %d \ndistinct: %d \nmin: %d\nmax: %d",
                 getSortedListInCorrectFormat(list),
                 getCount(list),
@@ -16,30 +26,32 @@ public class ListInfo {
                 getMax(list));
     }
 
+    //I decided to return sorted list in correct format.
     private String getSortedListInCorrectFormat(List<Integer> list) {
+        return list.stream() //start stream
+                .sorted() //sorts values
+                .map(Object::toString) //maps every int value to string
+                .collect(Collectors.joining(" ")); //collects mapped object to single String with joining method
+        // from Collectors.
+    }
+
+    private int getCount(List<Integer> list) {
+        return list.size(); // Are You really reading this? Go to task 3 already
+    }
+
+    private long getCountOfDistinctElements(List<Integer> list) {
+        return list.stream().distinct().count(); //.count returns long value, I could convert to int with return (int) list...
+    }
+
+    private long getMin(List<Integer> list) {
         return list.stream()
-                .sorted()
-                .map(Object::toString)
-                .collect(Collectors.joining(" "));
+                .min(Comparator.comparingInt(i -> i)) //finding min values using Comparator
+                .orElseThrow(InputMismatchException::new); //throws exception in case there's nothing to compare
     }
 
-    private int getCount (List<Integer> list) {
-        return list.size();
-    }
-
-    private long getCountOfDistinctElements (List<Integer> list) {
-        return list.stream().distinct().count();
-    }
-
-    private long getMin (List<Integer> list) {
+    private long getMax(List<Integer> list) {
         return list.stream()
-                .min(Comparator.comparingInt(i ->i))
-                .orElseThrow(InputMismatchException::new);
-    }
-
-    private long getMax (List<Integer> list) {
-        return list.stream()
-                .max(Comparator.comparingInt(i ->i))
-                .orElseThrow(InputMismatchException::new);
+                .max(Comparator.comparingInt(i -> i))//finding max values using Comparator
+                .orElseThrow(InputMismatchException::new);//throws exception in case there's nothing to compare
     }
 }
--- src/task1/Task1.java
+++ src/task1/Task1.java
@@ -7,8 +7,9 @@ import java.util.List;
 public class Task1 {
 
     public static void main(String[] args) {
+        //Create List containing Integer with usage of InputParserList
         List<Integer> inputList = InputParser.getIntegerListFromInput();
-        ListInfo listInfo = new ListInfo();
-        System.out.println(listInfo.getAllInfo(inputList));
+        ListInfo listInfo = new ListInfo(); //Create new ListInfo class
+        System.out.println(listInfo.getAllInfo(inputList));//Print basic info in expected format
     }
 }
